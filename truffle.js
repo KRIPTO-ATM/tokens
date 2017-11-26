b4aa3e4d9b7cd9635d567217096389815d396364
Frank Bonnet <frank@dcorp.it>
2017-11-26 07:54:39 +0100

    Added main network config

--- truffle.js
+++ truffle.js
@@ -5,6 +5,12 @@ module.exports = {
       port: 8545,
       gas: 6000000,
       network_id: "*" // Match any network id
+    },
+    main: {
+      host: "localhost",
+      port: 8547,
+      network_id: 1, // Official Ethereum network 
+      from: "0x3cAf983aCCccc2551195e0809B7824DA6FDe4EC8"
     }
   },
   solc: {
